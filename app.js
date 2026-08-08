a861d4c019feaec833e70ce75038c5bbd870980c
Shiva Marya <maryashiva07@gmail.com>
2026-08-08 12:14:41 +0530

    first commit

--- app.js
+++ app.js
@@ -1,8 +1,41 @@
+// Challenge- Create a Server with Middleware for Logging
+
 const express = require("express");
 const app = express();
 
-const port = 5666;
+
+// custom logger
+
+const logger = (req, res, next) =>{
+     console.log(`${req.method} request made to ${req.url}`);
+     next();
+}
+
+app.use(logger);
+
+//get
+app.get("/products", (req, res)=>{
+      console.log("Here is the list of all products.");
+});
+
+//post products
+app.post("/products", (req, res)=>{
+     console.log("A new product has been added.");
+});
+
+//get categories
+app.get("/categories", (req, res)=>{
+     console.log("Here is the list of all categories.");
+});
+
+//post
+app.post("/categories", (req, res)=>{
+     console.log("A new category has been created.");
+});
+
+
+const port = 4000;
 
 app.listen(port, ()=>{
-     console.log(`Server is up and running on Port ${port}! Ready to handle requests`);
+     console.log("App is running on Port: ", port);
 })
